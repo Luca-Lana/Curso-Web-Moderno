e4e33e1f047ce7b8122e99027b64935a8a402d16
Luca <lucalana13@gmail.com>
2023-04-05 11:32:54 -0300

    tagged template1

---
+++ 11. ESNext/taggedTemplate1.js
@@ -0,0 +1,11 @@
+// TAGGED TEMPLATE - PROCESSA TEMPLATE DENTRO DE UMA FUNÇÃO 
+function tag(partes, ...valores) {
+    console.log(partes);
+    console.log(valores);
+    return 'Outra string'
+}
+
+const aluno = 'Gui'
+const situacao = 'Aprovado'
+
+console.log(tag `${aluno} está ${situacao}`);
