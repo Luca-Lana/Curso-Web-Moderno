d51db38fc3af3c39a92e4c918e8cb6b2b3128aa4
Luca <lucalana13@gmail.com>
2023-04-05 12:14:27 -0300

    forOF

---
+++ 11. ESNext/forOf.js
@@ -0,0 +1,11 @@
+let nome = 'Luca'
+
+for (letra of nome) {
+    console.log(letra);
+}
+
+let assuntos = ['Internet', 'Jogos', 'Musicas']
+
+for (assunto of assuntos) {
+    console.log(assunto);
+}
