b0265fa2ee2b8d01cae5530160dd3fb831539f4f
Luca <lucalana13@gmail.com>
2023-04-05 17:29:29 -0300

    usando callback

--- 11. ESNext/usandoCallback.js
+++ 11. ESNext/usandoCallback.js
@@ -16,3 +16,16 @@ const getTurma = (letra, callback) => {
 
     })
 }
+
+let nomes = []
+
+getTurma('A', (alunos) => {
+    nomes = nomes.concat(alunos.map((a => `A: ${a.nome}`)))
+    getTurma('B', (alunos) => {
+        nomes = nomes.concat(alunos.map((a => `B: ${a.nome}`)))
+        getTurma('C', (alunos) => {
+            nomes = nomes.concat(alunos.map((a => `C: ${a.nome}`)))
+            console.log(nomes);
+        })
+    })
+})
