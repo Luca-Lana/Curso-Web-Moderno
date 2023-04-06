6f28504fd16a07d738968d5c92c77bc490e79098
Luca <lucalana13@gmail.com>
2023-04-05 22:22:31 -0300

    forma de resolver pelo curso

--- 11. ESNext/promise_desafio.js
+++ 11. ESNext/promise_desafio.js
@@ -7,5 +7,16 @@ const lerArquivo = (nomeArquivo) => {
     })
 }
 
-
 lerArquivo('dados.txt').then(conteudo => console.log(conteudo))
+
+// FORMA DE RESOLVER FEITO NO CURSO
+
+const lerArquivoCurso = (nomeArquivo) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(__dirname + '/'+ nomeArquivo, 'utf-8', (erro, data) => {
+            resolve(data)
+        })
+    })
+}
+
+lerArquivoCurso('dados.txt').then(conteudo => console.log(conteudo))
