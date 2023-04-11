4d526ace78b8b250d45233b2643e824f4b5a0961
Luca <lucalana13@gmail.com>
2023-04-11 18:18:55 -0300

    ajax 2

--- 16. AJax/server.js
+++ 16. AJax/server.js
@@ -9,6 +9,26 @@ app.use(express.static('.'))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './upload')
+    },
+    filename: function (req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload =  multer({storage}).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if (err) {
+            return res.end('Ocorreu um erro')
+        }
+        return res.end('Concluido com sucessp')
+    })
+})
+
 app.listen(8080, () => {
     console.log('Servidor rodando');
 })
