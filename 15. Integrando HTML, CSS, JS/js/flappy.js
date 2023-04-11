ef35edad20cfe0a3a01316418141cf90b4164157
Luca <lucalana13@gmail.com>
2023-04-11 15:06:28 -0300

    flappy bird 4

--- 15. Integrando HTML, CSS, JS/js/flappy.js
+++ 15. Integrando HTML, CSS, JS/js/flappy.js
@@ -71,8 +71,39 @@ function Barreiras (altura, largura, abertura, espaco, notificarPonto) {
     }
 }
 
+function Passaro(alturaJogo) {
+    let voando = false 
+
+    this.elemento = novoElemento('img', 'passaro')
+    this.elemento.src = 'imgs/passaro.png'
+
+    this.getY = () => parseInt(this.elemento.style.bottom.split('px')[0])
+    this.setY = y => this.elemento.style.bottom = `${y}px`
+
+    window.onkeydown = e => voando = true
+    window.onkeyup = e => voando = false
+
+    this.animar = () => {
+        const novoY =  this.getY() + (voando ? 7 : -6)
+        const alturaMaxima = alturaJogo - this.elemento.clientHeight
+        
+        if (novoY <= 0) {
+            this.setY(0)
+        } else if (novoY >= alturaMaxima) {
+            this.setY(alturaMaxima)
+        } else {
+            this.setY(novoY)
+        }
+    }
+
+    this.setY(alturaJogo / 2)
+}
+
 const barreiras = new Barreiras(700, 1200, 200, 400)
+const passaro = new Passaro(700)
+
 const areaJOgo = document.querySelector('[wm-flappy]')
+areaJOgo.appendChild(passaro.elemento)
 
 barreiras.pares.forEach(par => {
     areaJOgo.appendChild(par.elemento)
@@ -80,4 +111,5 @@ barreiras.pares.forEach(par => {
 
 setInterval(() => {
     barreiras.animar()
+    passaro.animar()
 }, 20)
