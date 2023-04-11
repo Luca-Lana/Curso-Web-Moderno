46a508001b30686a5ced6be3af5076f10b7f4873
Luca <lucalana13@gmail.com>
2023-04-11 17:00:31 -0300

    servidor criado

---
+++ 16. AJax/server.js
@@ -0,0 +1,19 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const multer = require('multer')
+
+const app = express()
+
+// CONFIGURAÇÕES
+app.use(express.static('.'))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+
+app.get('/teste', (req, res) => {
+    res.send('Ok')
+})
+
+app.listen(8080, () => {
+    console.log('Servidor rodando');
+})
